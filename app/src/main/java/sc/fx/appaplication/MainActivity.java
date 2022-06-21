c032e1c4cfe3456dc9d5f80129ff62732b4f630a
DrSkyFox <52832487+DrSkyFox@users.noreply.github.com>
2022-06-22 01:35:55 +0300

    add second_page

--- app/src/main/java/sc/fx/appaplication/MainActivity.java
+++ app/src/main/java/sc/fx/appaplication/MainActivity.java
@@ -1,5 +1,10 @@
 package sc.fx.appaplication;
 
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v("MainActivity", "onCreate");
+
+        EditText editText = findViewById(R.id.input_text_main);
+        Button getText = findViewById(R.id.button);
+        TextView textView = findViewById(R.id.textView);
+        getText.setOnClickListener(v -> {
+            String text = editText.getText().toString();
+            textView.setText(text);
+        });
+
     }
 
 

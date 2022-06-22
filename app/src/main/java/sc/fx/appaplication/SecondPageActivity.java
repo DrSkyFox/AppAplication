7524a026889ca36ab523338aaa6ab7c172a06758
DrSkyFox <52832487+DrSkyFox@users.noreply.github.com>
2022-06-23 01:22:24 +0300

    add second_page

---
+++ app/src/main/java/sc/fx/appaplication/SecondPageActivity.java
@@ -0,0 +1,13 @@
+package sc.fx.appaplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_page);
+    }
+}
